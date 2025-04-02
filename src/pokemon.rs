229bb0d5c01b64edc8c400609c0fa3c44b7c7a60
greg <gregory.tuck@hotmail.com>
2025-04-02 18:31:40 +0100

    Add type effectiveness matrix to Poketype enum

--- src/pokemon.rs
+++ src/pokemon.rs
@@ -33,8 +33,7 @@ impl Pokemon {
             return Ok(false);
         }
 
-        let damage = 1;
-        target.current_hp -= damage;
+        target.current_hp -= Self::calculate_damage(&attacking_move, &self, &target);
 
         self.level_up();
 
@@ -58,6 +57,14 @@ impl Pokemon {
     pub fn colored_name(&self) -> ColoredString {
         let color = self.poketype.color();
         self.name.truecolor(color.0, color.1, color.2)
+    }
+}
+
+impl Pokemon {
+    fn calculate_damage(attacking_move: &Pokemove, attacker: &Pokemon, target: &Pokemon) -> u16 {
+        let damage: u16 = attacking_move.power;
+
+        damage
     }
 }
 
@@ -92,6 +99,188 @@ pub enum Poketype {
 }
 
 impl Poketype {
+    fn effectiveness(&self, other: &Self) -> f32 {
+        match self {
+            Self::Normal => match other {
+                Self::Rock => 0.5,
+                Self::Ghost => 0.0,
+                Self::Steel => 0.5,
+                _ => 1.0,
+            },
+            Self::Fire => match other {
+                Self::Fire => 0.5,
+                Self::Water => 0.5,
+                Self::Grass => 2.0,
+                Self::Ice => 2.0,
+                Self::Bug => 2.0,
+                Self::Rock => 0.5,
+                Self::Dragon => 0.5,
+                Self::Steel => 2.0,
+                _ => 1.0,
+            },
+            Self::Water => match other {
+                Self::Fire => 2.0,
+                Self::Water => 0.5,
+                Self::Grass => 0.5,
+                Self::Electric => 1.0,
+                Self::Ice => 0.5,
+                Self::Steel => 0.5,
+                Self::Rock => 2.0,
+                Self::Dragon => 0.5,
+                _ => 1.0,
+            },
+            Self::Grass => match other {
+                Self::Fire => 0.5,
+                Self::Water => 0.5,
+                Self::Grass => 0.5,
+                Self::Electric => 1.0,
+                Self::Ice => 1.0,
+                Self::Poison => 0.5,
+                Self::Ground => 2.0,
+                Self::Flying => 0.5,
+                Self::Bug => 0.5,
+                Self::Rock => 2.0,
+                Self::Dragon => 0.5,
+                Self::Steel => 0.5,
+                _ => 1.0,
+            },
+            Self::Electric => match other {
+                Self::Water => 2.0,
+                Self::Electric => 0.5,
+                Self::Grass => 0.5,
+                Self::Ground => 2.0,
+                Self::Flying => 2.0,
+                Self::Dragon => 0.5,
+                _ => 1.0,
+            },
+            Self::Ice => match other {
+                Self::Fire => 0.5,
+                Self::Water => 0.5,
+                Self::Grass => 2.0,
+                Self::Ice => 0.5,
+                Self::Ground => 2.0,
+                Self::Flying => 2.0,
+                Self::Dragon => 2.0,
+                _ => 1.0,
+            },
+            Self::Fighting => match other {
+                Self::Normal => 2.0,
+                Self::Ice => 2.0,
+                Self::Rock => 2.0,
+                Self::Dark => 2.0,
+                Self::Steel => 2.0,
+                Self::Poison => 0.5,
+                Self::Flying => 0.5,
+                Self::Psychic => 0.5,
+                Self::Bug => 0.5,
+                Self::Fairy => 0.5,
+                Self::Ghost => 0.0,
+                _ => 1.0,
+            },
+            Self::Poison => match other {
+                Self::Grass => 2.0,
+                Self::Fairy => 2.0,
+                Self::Poison => 0.5,
+                Self::Ground => 0.5,
+                Self::Rock => 0.5,
+                Self::Ghost => 0.5,
+                Self::Steel => 0.0,
+                _ => 1.0,
+            },
+            Self::Ground => match other {
+                Self::Fire => 2.0,
+                Self::Electric => 2.0,
+                Self::Poison => 2.0,
+                Self::Rock => 2.0,
+                Self::Steel => 2.0,
+                Self::Grass => 0.5,
+                Self::Bug => 0.5,
+                Self::Flying => 0.0,
+                _ => 1.0,
+            },
+            Self::Flying => match other {
+                Self::Grass => 2.0,
+                Self::Fighting => 2.0,
+                Self::Bug => 2.0,
+                Self::Electric => 0.5,
+                Self::Rock => 0.5,
+                Self::Steel => 0.5,
+                _ => 1.0,
+            },
+            Self::Psychic => match other {
+                Self::Fighting => 2.0,
+                Self::Poison => 2.0,
+                Self::Psychic => 0.5,
+                Self::Steel => 0.5,
+                Self::Dark => 0.0,
+                _ => 1.0,
+            },
+            Self::Bug => match other {
+                Self::Grass => 2.0,
+                Self::Psychic => 2.0,
+                Self::Dark => 2.0,
+                Self::Fire => 0.5,
+                Self::Fighting => 0.5,
+                Self::Poison => 0.5,
+                Self::Flying => 0.5,
+                Self::Ghost => 0.5,
+                Self::Steel => 0.5,
+                Self::Fairy => 0.5,
+                _ => 1.0,
+            },
+            Self::Rock => match other {
+                Self::Fire => 2.0,
+                Self::Ice => 2.0,
+                Self::Flying => 2.0,
+                Self::Bug => 2.0,
+                Self::Fighting => 0.5,
+                Self::Ground => 0.5,
+                Self::Steel => 0.5,
+                _ => 1.0,
+            },
+            Self::Ghost => match other {
+                Self::Psychic => 2.0,
+                Self::Ghost => 2.0,
+                Self::Dark => 0.5,
+                Self::Normal => 0.0,
+                _ => 1.0,
+            },
+            Self::Dragon => match other {
+                Self::Dragon => 2.0,
+                Self::Steel => 0.5,
+                Self::Fairy => 0.0,
+                _ => 1.0,
+            },
+            Self::Dark => match other {
+                Self::Psychic => 2.0,
+                Self::Ghost => 2.0,
+                Self::Fighting => 0.5,
+                Self::Dark => 0.5,
+                Self::Fairy => 0.5,
+                _ => 1.0,
+            },
+            Self::Steel => match other {
+                Self::Ice => 2.0,
+                Self::Rock => 2.0,
+                Self::Fairy => 2.0,
+                Self::Fire => 0.5,
+                Self::Water => 0.5,
+                Self::Electric => 0.5,
+                Self::Steel => 0.5,
+                _ => 1.0,
+            },
+            Self::Fairy => match other {
+                Self::Fighting => 2.0,
+                Self::Dragon => 2.0,
+                Self::Dark => 2.0,
+                Self::Fire => 0.5,
+                Self::Poison => 0.5,
+                Self::Steel => 0.5,
+                _ => 1.0,
+            },
+        }
+    }
+
     fn color(&self) -> (u8, u8, u8) {
         match self {
             Self::Normal => (168, 167, 122),
