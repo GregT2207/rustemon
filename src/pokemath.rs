214eebb2253ca181aa1c7554e32d718c7b20d7af
greg <gregory.tuck@hotmail.com>
2025-04-03 16:36:38 +0100

    Add unit tests

--- src/pokemath.rs
+++ src/pokemath.rs
@@ -9,6 +9,27 @@ impl Percentage {
 
 impl Percentage {
     pub fn new(value: u8) -> Self {
-        Percentage(value.clamp(0, 100))
+        Percentage(value.clamp(0, 100) as u8)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn value_within_acceptable_range_remains_the_same() {
+        let value = 48;
+        let percentage = Percentage::new(value);
+
+        assert_eq!(percentage.value(), value as u8);
+    }
+
+    #[test]
+    fn value_above_100_clamps_to_100() {
+        let value = 204;
+        let percentage = Percentage::new(value);
+
+        assert_eq!(percentage.value(), 100);
     }
 }
